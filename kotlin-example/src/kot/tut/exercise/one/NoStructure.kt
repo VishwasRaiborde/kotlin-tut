ff4bc14e1965efe3a528ea2a17c2df4442008268
VishwasRaiborde <42356895+VishwasRaiborde@users.noreply.github.com>
2018-08-22 01:00:39 +0530

    first kotlin class the unstructured class the way we java guys see it now

---
+++ kotlin-example/src/kot/tut/exercise/one/NoStructure.kt
@@ -0,0 +1,13 @@
+package kot.tut.exercise.one
+
+fun printSomething (){
+	println("hello world please stop using plastic , primitive life was the best")
+}
+
+fun callPrintFunction (){
+	printSomething()
+}
+
+fun  main (args: Array<String>){
+	callPrintFunction()
+}
